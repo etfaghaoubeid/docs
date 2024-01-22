0127acd79d7d142585bfa2e28ab702e8588e88ed
Etfagha oubeid <Etfaghaoubeid@gamil.com>
2024-01-22 11:15:42 +0100

    remove tsc from build script

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { Landing } from "./landing";
 import { Rangement } from "./modules/rangement/pages";
